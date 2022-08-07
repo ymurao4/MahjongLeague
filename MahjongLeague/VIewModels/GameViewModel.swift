c01f4203e412d37ce417a2016b3c30f7bf0ccb5e
村尾慶伸 <muraoyoshinobu@muraokeishins-Mac-Studio.local>
2022-08-07 11:52:27 +0900

    create gameViewModel, add addGame method ;

---
+++ MahjongLeague/VIewModels/GameViewModel.swift
@@ -0,0 +1,22 @@
+import Foundation
+import Combine
+
+class GameViewModel: ObservableObject {
+    
+    @Published var gameRepository = GameRepository()
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        
+//        gameRepository.$game.map { games in
+//
+//
+//        }
+    }
+    
+    func addGame(game: Game) {
+        
+        gameRepository.addGame(game: game)
+    }
+}
