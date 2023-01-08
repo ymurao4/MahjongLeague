0404848127815853849bbee48427e0051c5ffcae
村尾慶伸 <muraoyoshinobu@muraokeishins-Mac-Studio-5.local>
2023-01-08 14:26:02 +0900

    close keyboard

--- MahjongLeague/GameFeature/AddGameCore.swift
+++ MahjongLeague/GameFeature/AddGameCore.swift
@@ -1,29 +1,50 @@
 import ComposableArchitecture
 import Foundation
 
+enum AddGameError: Error {
+    case playerCount
+    case totalScore
+    case emptyScore
+}
+
 struct AddGameFeature: ReducerProtocol {
     @Dependency(\.firebaseAPIClient) private var firebaseAPIClient
     struct State: Equatable {
+        @BindableState var focusedField: Field?
+        var date: Date = Date.now
         var players: [Player] = []
-        var peopleCount: Int = 4
+        var playerCount: Int = 4
         var gameType: GameType = .oneThree
+        var isHalfGame: Bool = true
         var scores: [Record.Score] = []
         var scoreFields: [String] = []
+        var errorType: AddGameError?
+        
+        enum Field: String, Hashable {
+            case score
+        }
     }
-
-    enum Action {
+    
+    enum Action: BindableAction, Equatable {
         case onAppear
         case loadPlayers
         case playerResponse(TaskResult<PlayerResult>)
-        case addGame(Game)
-        case peopleCountChanged(Int)
+        case submitGame
+        case selectDate(Date)
+        case playerCountChanged(Int)
         case gameTypeChanged(GameType)
+        case isHalfGameChanged(Bool)
         case addScore(Player)
-        case addScoreField(Int, String)
+        case removeScore(Int)
+        case scoreFieldChanged(Int, String)
+        case binding(BindingAction<State>)
+        case focusTextField
+        case unFocusTextField
     }
-
+    
     struct AddGameCancelId: Hashable {}
     var body: some ReducerProtocol<State, Action> {
+        BindingReducer()
         Reduce { state, action in
             switch action {
             case .onAppear:
@@ -41,16 +62,32 @@ struct AddGameFeature: ReducerProtocol {
             case .playerResponse(.failure(_)):
                 state.players = []
                 return .none
-            case let .addGame(game):
+            case .submitGame:
+                guard state.playerCount == state.scores.count else {
+                    state.errorType = .playerCount
+                    return .none
+                }
+                var game: Game = .init(
+                    result: Record(scores: state.scores),
+                    isHalfRound: state.isHalfGame,
+                    isFourPeople: state.playerCount == 4,
+                    gameType: state.gameType.rawValue
+                )
                 return .none
-            case let .peopleCountChanged(count):
-                state.peopleCount = count
+            case let .selectDate(date):
+                state.date = date
+                return .none
+            case let .playerCountChanged(count):
+                state.playerCount = count
                 return .none
             case let .gameTypeChanged(gameType):
                 state.gameType = gameType
                 return .none
+            case let .isHalfGameChanged(isHalfGame):
+                state.isHalfGame = isHalfGame
+                return .none
             case let .addScore(player):
-                if state.scores.count < state.peopleCount {
+                if state.scores.count < state.playerCount {
                     for addedScore in state.scores {
                         if addedScore.player.id == player.id {
                             return .none
@@ -60,8 +97,19 @@ struct AddGameFeature: ReducerProtocol {
                     state.scores.append(score)
                 }
                 return .none
-            case let .addScoreField(i, text):
+            case let .removeScore(i):
+                state.scores.remove(at: i)
+                return .none
+            case let .scoreFieldChanged(i, text):
                 state.scores[i].point = text
+                return .none
+            case .binding:
+                return .none
+            case .focusTextField:
+                state.focusedField = .score
+                return .none
+            case .unFocusTextField:
+                state.focusedField = nil
                 return .none
             }
         }
--- MahjongLeague/GameFeature/AddGameView.swift
+++ MahjongLeague/GameFeature/AddGameView.swift
@@ -3,6 +3,8 @@ import ComposableArchitecture
 
 struct AddGameView: View {
     let store: StoreOf<AddGameFeature>
+    @FocusState var focusedField: AddGameFeature.State.Field?
+
     private let columns: GridItem = .init(.fixed(56))
     
     init(store: StoreOf<AddGameFeature>) {
@@ -11,107 +13,126 @@ struct AddGameView: View {
     
     var body: some View {
         WithViewStore(self.store) { viewStore in
-            ZStack(alignment: .bottom) {
-                List {
-                    Section(header: HStack {
-                        Text("メンバー")
-                            .font(.title3)
-                            .bold()
-                        Spacer()
-                        Menu {
-                            Button("メンバーを追加", action: {  })
-                            Button("メンバーを編集", action: {  })
-                        } label: {
-                            Image(systemName: "ellipsis.circle")
-                        }
-                    }) {
-                        if viewStore.players.isEmpty {
-                            Text("右上の３点リーダーをタップし、メンバーを追加！")
-                                .font(.callout)
-                                .foregroundColor(.gray)
-                        } else {
-                            LazyVGrid(columns: Array(repeating: columns, count: 5)) {
-                                ForEach(viewStore.players) { player in
-                                    Button {
-                                        viewStore.send(.addScore(player))
-                                    } label: {
-                                        PlayerView(player: player)
+            NavigationStack {
+                ZStack(alignment: .bottom) {
+                    List {
+                        Section(header: HStack {
+                            Text("メンバー")
+                                .font(.title3)
+                                .bold()
+                            Spacer()
+                            Menu {
+                                Button("メンバーを追加", action: {  })
+                                Button("メンバーを編集", action: {  })
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
+                        }) {
+                            if viewStore.players.isEmpty {
+                                Text("右上の３点リーダーをタップし、メンバーを追加！")
+                                    .font(.callout)
+                                    .foregroundColor(.gray)
+                            } else {
+                                LazyVGrid(columns: Array(repeating: columns, count: 5)) {
+                                    ForEach(viewStore.players) { player in
+                                        Button {
+                                            viewStore.send(.addScore(player))
+                                        } label: {
+                                            PlayerView(player: player)
+                                        }
+                                        .buttonStyle(PlainButtonStyle())
                                     }
-                                    .buttonStyle(PlainButtonStyle())
                                 }
                             }
                         }
-                    }
-                    Section(header: Text("ルール").font(.title3).bold()) {
-                        Picker("", selection: viewStore.binding(
-                            get: \.peopleCount,
-                            send: AddGameFeature.Action.peopleCountChanged)
-                        ) {
-                            Text("四人麻雀")
-                                .tag(4)
-                            Text("三人麻雀")
-                                .tag(3)
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                        Picker("ウマを選択", selection: viewStore.binding(
-                            get: \.gameType,
-                            send: AddGameFeature.Action.gameTypeChanged)
-                        ) {
-                            ForEach(GameType.allCases, id: \.self) { value in
-                                Text(value.rawValue)
+                        Section(header: Text("ルール").font(.title3).bold()) {
+                            Picker("", selection: viewStore.binding(
+                                get: \.playerCount,
+                                send: AddGameFeature.Action.playerCountChanged)
+                            ) {
+                                Text("四人麻雀")
+                                    .tag(4)
+                                Text("三人麻雀")
+                                    .tag(3)
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                            Toggle("半荘", isOn: viewStore.binding(
+                                get: \.isHalfGame,
+                                send: AddGameFeature.Action.isHalfGameChanged)
+                            )
+                            DatePicker("日付を選択", selection: viewStore.binding(
+                                get: \.date,
+                                send: AddGameFeature.Action.selectDate)
+                                       , displayedComponents: .date)
+                            Picker("ウマを選択", selection: viewStore.binding(
+                                get: \.gameType,
+                                send: AddGameFeature.Action.gameTypeChanged)
+                            ) {
+                                ForEach(GameType.allCases, id: \.self) { value in
+                                    Text(value.rawValue)
+                                }
                             }
                         }
-                    }
-                    Section(header: Text("参加者").font(.title3).bold()) {
-                        if viewStore.scores.isEmpty {
-                            Text("画面上部のメンバーをタップして参加者を追加！")
-                                .font(.callout)
-                                .foregroundColor(.gray)
-
-                        } else {
-                            ForEach(0..<viewStore.scores.count, id:\.self) { i in
-                                HStack(spacing: 16) {
-                                    Text(viewStore.scores[i].player.name)
-                                    TextField("score", text: viewStore.binding(
-                                        get: \.scores[i].point,
-                                        send: { .addScoreField(i, $0) })
-                                    )
+                        Section(header: Text("参加者").font(.title3).bold()) {
+                            if viewStore.scores.isEmpty {
+                                Text("画面上部のメンバーをタップして参加者を追加！")
+                                    .font(.callout)
+                                    .foregroundColor(.gray)
+                                
+                            } else {
+                                ForEach(0..<viewStore.scores.count, id:\.self) { i in
+                                    HStack(spacing: 16) {
+                                        Text(viewStore.scores[i].player.name)
+                                        TextField("score", text: viewStore.binding(
+                                            get: \.scores[i].point,
+                                            send: { .scoreFieldChanged(i, $0) })
+                                        )
+                                        .onTapGesture {
+                                            viewStore.send(.focusTextField)
+                                        }
+                                        .focused($focusedField, equals: .score)
                                         .keyboardType(.numberPad)
+                                    }
+                                    .swipeActions(edge: .trailing) {
+                                        Button {
+                                            viewStore.send(.removeScore(i))
+                                        } label: {
+                                            Image(systemName: "trash")
+                                        }
+                                        .tint(.red)
+                                    }
                                 }
-//                                    .swipeActions(edge: .trailing) {
-//                                        Button {
-//                                            gameResults.remove(at: i)
-//                                        } label: {
-//                                            Image(systemName: "trash")
-//                                        }
-//                                        .tint(.red)
-//                                    }
                             }
                         }
                     }
                 }
-                Button {
-                    
-                } label: {
-                    Text("記録する")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 12)
-                        .foregroundColor(.white)
-                        .bold()
+                .navigationBarTitle("記録", displayMode: .inline)
+                .toolbar {
+                    Button {
+                        viewStore.send(.submitGame)
+                    } label: {
+                        Text("保存")
+                    }
                 }
-                .background(Color.primary)
-                .cornerRadius(4)
-                .padding(.horizontal, 16)
+                .onAppear {
+                    viewStore.send(.onAppear)
+                }
             }
-            .onAppear {
-                viewStore.send(.onAppear)
-            }
+            .synchronize(viewStore.binding(\.$focusedField), self.$focusedField)
+            .gesture(
+                DragGesture()
+                .onChanged{ value in
+                    if value.translation.height != 0 {
+                        viewStore.send(.unFocusTextField)
+                    }
+                }
+            )
         }
     }
-
+    
     private struct PlayerView: View {
         var player: Player
-
+        
         var body: some View {
             Text(player.name)
                 .foregroundColor(.primary)
@@ -126,4 +147,15 @@ struct AddGameView: View {
                 )
         }
     }
+}
+
+extension View {
+  func synchronize<Value>(
+    _ first: Binding<Value>,
+    _ second: FocusState<Value>.Binding
+  ) -> some View {
+    self
+      .onChange(of: first.wrappedValue) { second.wrappedValue = $0 }
+      .onChange(of: second.wrappedValue) { first.wrappedValue = $0 }
+  }
 }
